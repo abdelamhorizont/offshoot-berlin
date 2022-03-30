e2a146fee73f63f37365f6e8c7cc9f393c2d43d4
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-03-30 17:58:20 +0200

    about page

--- src/pages/meta/about.js
+++ src/pages/meta/about.js
@@ -1,13 +1,21 @@
 import * as React from "react"
+import { Link, useStaticQuery, graphql } from 'gatsby'
+
 import Layout from '../../components/layout/layout'
 
 export default function About() {
+    const data = useStaticQuery(graphql`
+    query {
+        wpPage(title: {eq: "About"}) {
+            content
+          }
+        }   
+    `)
 
     return (
         <Layout>
-            <h1>
-                About
-            </h1>
+      <div dangerouslySetInnerHTML={{ __html: data.wpPage.content }} />
+
         </Layout>
     )
 }
