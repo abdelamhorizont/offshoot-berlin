5d0ac25ce472aedafdf9cddd2e1e00ff20b8f23a
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-04-20 14:51:06 +0200

    readme

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,14 +4,14 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/404.js")),
-  "component---src-pages-about-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/about.js")),
-  "component---src-pages-agb-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/agb.js")),
-  "component---src-pages-contact-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/contact.js")),
-  "component---src-pages-imprint-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/imprint.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/index.js")),
-  "component---src-pages-work-all-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/workAll.js")),
-  "component---src-pages-wp-posturi-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin/src/pages/wpPosturi.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/about.js")),
+  "component---src-pages-agb-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/agb.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/contact.js")),
+  "component---src-pages-imprint-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/imprint.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/index.js")),
+  "component---src-pages-work-all-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/workAll.js")),
+  "component---src-pages-wp-posturi-js": preferDefault(require("/Users/abdelammeer/Creative Cloud Files/offshoot-berlin/offshoot-berlin-website/src/pages/wpPosturi.js"))
 }
 
