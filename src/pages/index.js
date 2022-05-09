1533103626be15a425cc6713accb7e3cbe6e42d1
abdelamhorizont <abdelhamid.ameur@hotmail.de>
2022-05-09 11:49:08 +0200

    mobile logo playsinline

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,10 @@ import '../styles/reset.scss'
 import '../styles/global.scss'
 import '../styles/typo.module.scss'
 
-export default function Home({location}) {
+export default function Home({ location }) {
+  // let vh = window.innerHeight * 0.01;
+  // document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   return (
     <div>
       <Layout path={location.pathname}>
